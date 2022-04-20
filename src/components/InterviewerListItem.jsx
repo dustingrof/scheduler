ddd0b90e999c201c2cd5431f4e5d204a963cfc81
Dustin Grof <dustingrof@gmail.com>
2022-04-20 14:46:08 -0700

    create Interviewer List Item

---
+++ src/components/InterviewerListItem.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+  const interviewerClass = classNames("interviewers__item", {
+    " interviewers__item--selected": props.selected,
+  });
+
+  return (
+    <li
+      className={interviewerClass}
+      id={props.id}
+      onClick={() => props.setInterviewer(props.id)}>
+      <img
+        src={props.avatar}
+        alt={props.name}
+        className="interviewers__item-image"
+      />
+      {(props.selected && props.name)}
+    </li>
+  );
+}
