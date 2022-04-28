6400a2bdce5ac5a51aac858951efeb76442f7802
Dustin Grof <dustingrof@gmail.com>
2022-04-27 17:48:28 -0700

    implement tests on form

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -12,7 +12,8 @@ export default function Button(props) {
       <button
         className={buttonClass}
         onClick={props.onClick}
-        disabled={props.disabled}>
+        disabled={props.disabled}
+        type={props.type} >
         {props.children}
       </button>
     </>
